dc29091ee64e2e7129c29c285eb06de5245cbdb9
TSITCL\s152133 <yukki334@gmail.com>
2017-09-13 21:47:37 +0900

    Arena Fixed

---
+++ Gladiatores/Assets/Scripts/Animation/bobbing.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bobbing : MonoBehaviour {
+
+    float time = 0;
+    public Animator myAnimator;
+   
+	// Use this for initialization
+	void Start () {
+        myAnimator.enabled = false;
+        time = Random.Range(0.0f, 3.0f);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        time -= Time.deltaTime;
+
+        if (time <= 0.0f)
+        {
+            myAnimator.enabled = true;
+        }
+    }
+}
