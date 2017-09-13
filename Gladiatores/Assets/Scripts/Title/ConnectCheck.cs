71fb4f67b8e06b755839650c1a6e02c1ec10e7e0
TakayoshiOgawa <ogawa4283gamecreator@gmail.com>
2017-09-14 04:42:25 +0900

    Fixed Ready Scene

--- Gladiatores/Assets/Scripts/Title/ConnectCheck.cs
+++ Gladiatores/Assets/Scripts/Title/ConnectCheck.cs
@@ -7,29 +7,29 @@ using UnityEngine.UI;
 
 public class ConnectCheck : MonoBehaviour {
 
-    private bool singleFlag;//Flag of play single
 
+    private bool singleFlag;//Flag of play single
     private bool multiFlag;//Flag of play multi
 
     [SerializeField]
     private Image OneState;//state of 1P
-
     [SerializeField]
     private Image TwoState;//state of 2P
-
     [SerializeField]
     private Image playButton;//Play Button
 
     //1P,2PのAボタンを押したフラグ
-    bool start1;
-    bool start2;
+    private bool start1;
+    private bool start2;
 
     //スプライトの切り替え
-    Texture2D textureBef;
-    Texture2D textureAfter;
+    [SerializeField]
+    private Sprite textureBef;
+    [SerializeField]
+    private Sprite textureAfter;
 
-    Sprite spBef;
-    Sprite spAf;
+    //Sprite spBef;
+    //Sprite spAf;
 
     //案内の点滅
     [SerializeField]
@@ -38,17 +38,6 @@ public class ConnectCheck : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
-        start1 = false;
-        start2 = false;
-
-        textureBef = Resources.Load("Textures/UI/Player_NotReady") as Texture2D;
-        textureAfter = Resources.Load("Textures/UI/Player_Ready") as Texture2D;
-
-        OneState = GameObject.Find("Canvas/SelectIcon/controller(1P)").GetComponent<Image>();
-        TwoState = GameObject.Find("Canvas/SelectIcon/controller(2P)").GetComponent<Image>();
-
-        spBef = Sprite.Create(textureBef, new Rect(0, 0, textureBef.width, textureBef.height), Vector2.zero);
-        spBef = Sprite.Create(textureAfter, new Rect(0, 0, textureAfter.width, textureAfter.height), Vector2.zero);
     }
 
     // Update is called once per frame
@@ -57,55 +46,84 @@ public class ConnectCheck : MonoBehaviour {
         //ガイドを点滅させる
         guide.color = new Color(guide.color.r, guide.color.g, guide.color.b, Mathf.PingPong(Time.time, 1f));
 
-        //Input.ResetInputAxes();
-        // 接続されているコントローラの名前を調べる
-        var controllerNames = Input.GetJoystickNames();
-        controllerNames.Initialize();
+        ////Input.ResetInputAxes();
+        //// 接続されているコントローラの名前を調べる
+        //var controllerNames = Input.GetJoystickNames();
+        //controllerNames.Initialize();
 
-        //1P Player Login
+        ////1P Player Login
         if (GamepadInput.GamePad.GetButtonDown(GamepadInput.GamePad.Button.Start, GamepadInput.GamePad.Index.One))
         {
-            //OneState.color = new Color(OneState.color.r, OneState.color.g, OneState.color.b, 255f);
-            OneState.sprite = spAf;
-            singleFlag = true;
+            start1 = !start1;
+            OneState.sprite = (start1) ? textureAfter : textureBef;
         }
+        //if (GamepadInput.GamePad.GetButtonDown(GamepadInput.GamePad.Button.Start, GamepadInput.GamePad.Index.One))
+        //{
+        //    //OneState.color = new Color(OneState.color.r, OneState.color.g, OneState.color.b, 255f);
+        //    OneState.sprite = spAf;
+        //    singleFlag = true;
+        //}
 
-        //2P Player Login
+        ////2P Player Login
         if (GamepadInput.GamePad.GetButtonDown(GamepadInput.GamePad.Button.Start, GamepadInput.GamePad.Index.Two))
         {
-            if (controllerNames.Length >= 2)
+            start2 = !start2;
+            TwoState.sprite = (start2) ? textureAfter : textureBef;
+        }
+        //if (GamepadInput.GamePad.GetButtonDown(GamepadInput.GamePad.Button.Start, GamepadInput.GamePad.Index.Two))
+        //{
+        //    if (controllerNames.Length >= 2)
+        //    {
+        //        multiFlag = true;
+        //    }
+        //    //TwoState.color = new Color(TwoState.color.r, TwoState.color.g, TwoState.color.b, 255f);
+        //    TwoState.sprite = spAf;
+        //}
+
+
+        //if (singleFlag == true)//1Pがエントリーしていたら
+        //{
+        //    //Display PlayButton(プレイボタンをアクティブにする)
+        //    playButton.color = new Color(playButton.color.r, playButton.color.g, playButton.color.b, 255f);
+        //}
+
+        ////Both player push "A". ->StartMultiPlay
+        //if (GamepadInput.GamePad.GetButtonDown(GamepadInput.GamePad.Button.A, GamepadInput.GamePad.Index.One)) start1 = true;
+        //if (GamepadInput.GamePad.GetButtonDown(GamepadInput.GamePad.Button.A, GamepadInput.GamePad.Index.Two)) start2 = true;
+
+        // プレイボタンのアクティブを開始フラグで決める
+        playButton.gameObject.SetActive((start1 || start2) ? true : false);
+
+        // ボタンが押されることでゲームを開始（フラグに応じて分岐）
+        if ((GamepadInput.GamePad.GetButtonDown(GamepadInput.GamePad.Button.A, GamepadInput.GamePad.Index.One)
+         || (GamepadInput.GamePad.GetButtonDown(GamepadInput.GamePad.Button.A, GamepadInput.GamePad.Index.Two))))
             {
-                multiFlag = true;
-            }
-            //TwoState.color = new Color(TwoState.color.r, TwoState.color.g, TwoState.color.b, 255f);
-            TwoState.sprite = spAf;
-        }
-
-
-        if (singleFlag == true)//1Pがエントリーしていたら
-        {
-            //Display PlayButton(プレイボタンをアクティブにする)
-            playButton.color = new Color(playButton.color.r, playButton.color.g, playButton.color.b, 255f);
-        }
-
-        //Both player push "A". ->StartMultiPlay
-        if (GamepadInput.GamePad.GetButtonDown(GamepadInput.GamePad.Button.A, GamepadInput.GamePad.Index.One)) start1 = true;
-        if (GamepadInput.GamePad.GetButtonDown(GamepadInput.GamePad.Button.A, GamepadInput.GamePad.Index.Two)) start2 = true;
-
-        //シングルプレイ
-        if (singleFlag == false || multiFlag == false)
-        {
-            if (start1 == true)
+            if (start1 && start2)
             {
-                Debug.Log("SINGLE");
+                UnityEngine.SceneManagement.SceneManager.LoadScene("arenaMulti");
             }
-        }
-        else//マルチプレイ
-        {
-            if (start1 == true && start2 == true)
+            else if (start1 || start2)
             {
-                Debug.Log("MULTI");
+                UnityEngine.SceneManagement.SceneManager.LoadScene("arenaSingle");
             }
         }
+
+        ////シングルプレイ
+        //if (singleFlag == false || multiFlag == false)
+        //{
+        //    if (start1 == true)
+        //    {
+        //        Debug.Log("SINGLE");
+        //        UnityEngine.SceneManagement.SceneManager.LoadScene("arenaSingle");
+        //    }
+        //}
+        //else//マルチプレイ
+        //{
+        //    if (start1 == true && start2 == true)
+        //    {
+        //        Debug.Log("MULTI");
+        //        UnityEngine.SceneManagement.SceneManager.LoadScene("arenaMulti");
+        //    }
+        //}
     }
 }
