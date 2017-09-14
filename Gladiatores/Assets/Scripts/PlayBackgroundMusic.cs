73e2ac8b4ca1100e7f25bf6000001d3d37d2b2cb
TakayoshiOgawa <ogawa4283gamecreator@gmail.com>
2017-09-14 16:47:02 +0900

    Maked BackgroundMusic Scripts

---
+++ Gladiatores/Assets/Scripts/PlayBackgroundMusic.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayBackgroundMusic : MonoBehaviour {
+
+    [SerializeField]
+    private string bgmName;
+
+	void Start () {
+        SoundManager.Instance.PlayBGM(bgmName);
+	}
+}
